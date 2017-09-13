b191873796a42fefb2f34b7db2ff33981c39e2dd
91078024 <31664275+91084970@users.noreply.github.com>
2017-09-13 08:18:48 -0700

    Initial commit

---
+++ src/Ch002.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ch002
+{
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter your first name:");
+    String name = scan.next();
+    System.out.println("Hello, " + name);
+  }
+}
